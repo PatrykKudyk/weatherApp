f1cc01d86e2d5917c1a52477f54bf3518b3daf54
pkudyk <gustaw5022@gmail.com>
2021-08-25 15:40:23 +0200

    added Animations but background animations still not always working properly

--- app/src/main/java/com/example/weatherapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.weatherapp.ui.home
 
 import android.os.Bundle
+import android.os.Handler
+import android.transition.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,10 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weatherapp.R
-import com.example.weatherapp.adapters.DaysRecyclerViewAdapter
-import com.example.weatherapp.adapters.HoursRecyclerViewAdapter
 import com.example.weatherapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -33,16 +32,28 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        viewModel.primaryCity.observe(requireActivity(), Observer{ city ->
-            city?.let{
+        viewModel.primaryCity.observe(requireActivity(), Observer { city ->
+            city?.let {
                 cityName = city.name
                 checkPrimaryCity()
+                makeStartAnimations()
             }
         })
-        checkPrimaryCity()
         initListeners()
 
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        makeStartAnimations()
+    }
+
+    override fun onStop() {
+        binding.mainConstraint.visibility = View.GONE
+        makeViewsGone()
+        viewModel.shouldAnimate = true
+        super.onStop()
     }
 
     override fun onDestroyView() {
@@ -52,26 +63,115 @@ class HomeFragment : Fragment() {
 
     private fun checkPrimaryCity() {
         if (cityName.isEmpty()) {
-            binding.addCityGroup.visibility = View.VISIBLE
-            binding.showWeatherGroup.visibility = View.GONE
+            binding.addCityLayout.visibility = View.VISIBLE
+            binding.mainConstraint.visibility = View.GONE
+            binding.fabSwap.visibility = View.GONE
+
         } else {
+
             binding.textViewCity.text = cityName
             viewModel.fetchForecastData(cityName, requireActivity(), binding)
-            binding.showWeatherGroup.visibility = View.VISIBLE
-            binding.addCityGroup.visibility = View.GONE
+
+            binding.addCityLayout.visibility = View.GONE
+            binding.mainConstraint.visibility = View.VISIBLE
+            binding.fabSwap.visibility = View.VISIBLE
         }
     }
 
     private fun initListeners() {
-        val fab = binding.fabSwap
-        val addButton = binding.imageAddCity
-
-        fab.setOnClickListener {
+        binding.fabSwap.setOnClickListener {
             findNavController().navigate(R.id.action_nav_home_to_add_city)
         }
 
-        addButton.setOnClickListener {
+        binding.imageAddCity.setOnClickListener {
             findNavController().navigate(R.id.action_nav_home_to_add_city)
+        }
+
+        binding.fabAnimation.setOnClickListener {
+            makeStartAnimations()
         }
     }
+
+    private fun makeStartAnimations() {
+        if (viewModel.shouldAnimate) {
+            binding.mainConstraint.visibility = View.GONE
+            makeViewsGone()
+
+            Handler().postDelayed({
+                if (_binding != null)
+                    animateBackground()
+            }, 50)
+
+            Handler().postDelayed({
+                if (_binding != null)
+                    animateWeather()
+            }, 1600)
+
+            viewModel.shouldAnimate = false
+        }
+    }
+
+
+    private fun animateBackground() {
+        val tBackground = TransitionInflater.from(requireContext())
+            .inflateTransition(R.transition.transition_weather_background_showup)
+
+        TransitionManager.beginDelayedTransition(binding.mainConstraint, tBackground)
+
+        binding.mainConstraint.visibility = View.VISIBLE
+
+        Handler().postDelayed({
+            if (_binding != null) {
+                TransitionManager.endTransitions(binding.mainConstraint)
+            }
+        }, 1500)
+    }
+
+    private fun animateWeather() {
+        val tCityName = TransitionInflater.from(requireContext())
+            .inflateTransition(R.transition.transition_city_name)
+        val tCityDesc = TransitionInflater.from(requireContext())
+            .inflateTransition(R.transition.transition_city_details)
+        val tCityRecyclers = TransitionInflater.from(requireContext())
+            .inflateTransition(R.transition.transition_city_forecast)
+
+        TransitionManager.beginDelayedTransition(binding.cityNameConstraint, tCityName)
+        TransitionManager.beginDelayedTransition(binding.conditionConstraint, tCityDesc)
+        TransitionManager.beginDelayedTransition(binding.recyclersConstraint, tCityRecyclers)
+
+        makeViewsVisible()
+
+        Handler().postDelayed({
+            if (_binding != null) {
+                TransitionManager.endTransitions(binding.cityNameConstraint)
+                TransitionManager.endTransitions(binding.conditionConstraint)
+                TransitionManager.endTransitions(binding.recyclersConstraint)
+            }
+        }, 1000)
+    }
+
+    private fun makeViewsGone() {
+        binding.textViewCity.visibility = View.GONE
+        binding.textViewTemperature.visibility = View.GONE
+        binding.textViewDescription.visibility = View.GONE
+        binding.textViewHighTemperature.visibility = View.GONE
+        binding.textViewLowTemperature.visibility = View.GONE
+        binding.recyclerTopSeparator.visibility = View.GONE
+        binding.recyclerBottomSeparator.visibility = View.GONE
+        binding.recyclerHours.visibility = View.GONE
+        binding.recyclerDays.visibility = View.GONE
+    }
+
+    private fun makeViewsVisible() {
+        binding.textViewCity.visibility = View.VISIBLE
+        binding.textViewTemperature.visibility = View.VISIBLE
+        binding.textViewDescription.visibility = View.VISIBLE
+        binding.textViewHighTemperature.visibility = View.VISIBLE
+        binding.textViewLowTemperature.visibility = View.VISIBLE
+        binding.recyclerTopSeparator.visibility = View.VISIBLE
+        binding.recyclerBottomSeparator.visibility = View.VISIBLE
+        binding.recyclerHours.visibility = View.VISIBLE
+        binding.recyclerDays.visibility = View.VISIBLE
+    }
+
 }
